e7ade9055ed97fc05e5441148873cbf9c4c02786
jameschristou <james.christou@gmail.com>
2019-12-19 14:49:29 +1100

    Got a working version of the multiselect items

--- src/components/AutoCompleteTextBox.js
+++ src/components/AutoCompleteTextBox.js
@@ -8,7 +8,7 @@ const AutoCompleteTextBox = ({label, items, updateListHandler, filterOptionsHand
 
   useEffect(() => {
     console.log('useEffect:AutoCompleteTextBox');
-    itemsContainer.current.addEventListener("blur", (evnt) => console.log("Blur event triggered"));
+    //itemsContainer.current.addEventListener("blur", (evnt) => console.log("Blur event triggered"));
   }, [isAddingNewItem]);
 
   const newItemUpdatedEventHandler = (evnt) => {
@@ -16,7 +16,8 @@ const AutoCompleteTextBox = ({label, items, updateListHandler, filterOptionsHand
   }
 
   const addNewItemHandler = (evnt) => {
-    console.log('Adding new item');
+    evnt.stopPropagation();
+    console.log('addNewItemHandler handler');
 
     if(!isAddingNewItem){
       setIsAddingNewItem(true);
@@ -24,6 +25,8 @@ const AutoCompleteTextBox = ({label, items, updateListHandler, filterOptionsHand
     else{
       setIsAddingNewItem(false);
     }
+
+    return false;
   }
 
   const addItemHandler = (item) => {
@@ -39,6 +42,8 @@ const AutoCompleteTextBox = ({label, items, updateListHandler, filterOptionsHand
   }
 
   const newItemBlurEventHandler = (evnt) => {
+    evnt.stopPropagation();
+
     setIsAddingNewItem(false);
     console.log('newItemBlurEventHandler');
   }
@@ -56,7 +61,7 @@ const AutoCompleteTextBox = ({label, items, updateListHandler, filterOptionsHand
   return (
     <div className="autocompleteTextBox">
       <label className="autocompleteTextBox__label">{label}</label>
-      <div ref={itemsContainer} className="autocompleteTextBox__items" onClick={evnt => addNewItemHandler()} >
+      <div ref={itemsContainer} className="autocompleteTextBox__items" onClick={evnt => {evnt.stopPropagation(); addNewItemHandler(evnt);}} >
         {items.map(
           (item, index) => {
             return (
@@ -64,10 +69,10 @@ const AutoCompleteTextBox = ({label, items, updateListHandler, filterOptionsHand
             );
           }
         )}
-        {isAddingNewItem &&
+        {/* {isAddingNewItem &&
           <NewItem filterOptionsHandler={filterOptionsHandler} existingItems={items} selectNewItemHandler={addItemHandler} onBlurHandler={newItemBlurEventHandler}/>
-        }
-        {/* <NewItem filterOptionsHandler={filterOptionsHandler} existingItems={items} selectNewItemHandler={addItemHandler} onBlurHandler={newItemBlurEventHandler}/> */}
+        } */}
+        <NewItem filterOptionsHandler={filterOptionsHandler} existingItems={items} selectNewItemHandler={addItemHandler} onBlurHandler={newItemBlurEventHandler} shouldShow={isAddingNewItem}/>
       </div>
     </div>
   );
@@ -82,21 +87,32 @@ const AutoCompleteItem = ({itemIndex, item, removeItemHandler}) => {
   );
 }
 
-const NewItem = ({filterOptionsHandler, existingItems, selectNewItemHandler, onBlurHandler}) => {
+const NewItem = ({filterOptionsHandler, existingItems, selectNewItemHandler, onBlurHandler, shouldShow}) => {
   const txtBoxRef = useRef(null);
   const [parentHeight, setParentHeight] = useState(0);
   const [parentWidth, setParentWidth] = useState(0);
+  const [clickOutEventRegistered, setClickOutEventRegistered] = useState(0);
   const [newItemsOptions, setNewItemsOptions] = useState([]);
 
   useEffect(() => {
-    console.log('useEffect:NewItem');
+    console.log('useEffect:NewItem' + ' shouldShow:' + shouldShow);
     txtBoxRef.current.focus();
 
     setParentHeight(txtBoxRef.current.clientHeight);
     setParentWidth(txtBoxRef.current.clientWidth);
 
     setNewItemsOptions(getNotSelectedOptions(''));
-  }, [txtBoxRef]);
+
+    if(shouldShow && !clickOutEventRegistered){
+      document.addEventListener("click", documentClickEventListener);
+
+      setClickOutEventRegistered(true);
+    }
+    else{
+      document.removeEventListener("click", documentClickEventListener);
+    }
+
+  }, [txtBoxRef, shouldShow]);
 
   const newItemUpdatedEventHandler = (evnt) => {
     let options = getNotSelectedOptions(evnt.target.textContent.trim());
@@ -108,18 +124,50 @@ const NewItem = ({filterOptionsHandler, existingItems, selectNewItemHandler, onB
     let options = filterOptionsHandler(filterText);
 
     // remove options which are already selected
-    return options.filter(o => existingItems.indexOf(o) < 0);
+    let notSelectedOptions = options.filter(o => existingItems.indexOf(o) < 0);
+
+    if(notSelectedOptions.length == 0){
+      return ["No further options available"];
+    }
+
+    return notSelectedOptions;
+  }
+
+  const documentClickEventListener = (evnt) => {
+    console.log('click outside listener');
+
+    let clickOutElement = document.querySelector(".autocompleteTextBoxNewItem");
+
+    if(clickOutElement == null) return;
+
+    if(clickOutElement.className.includes('hidden')) return;
+
+    if(evnt.target == document.querySelector(".autocompleteTextBox__items")) return;
+
+    let targetElement = evnt.target;
+
+    do {
+      if (targetElement == clickOutElement) {
+          return;
+      }
+      // Go up the DOM.
+      targetElement = targetElement.parentNode;
+    } while (targetElement);
+
+    console.log('Calling onBlur handler');
+
+    onBlurHandler(evnt);
   }
 
   return (
-    <div className="autocompleteTextBoxNewItem" style={{width:parentWidth, height:parentHeight}}>
-      <div className="autocompleteTextBoxNewItem__container" onBlur={evnt => {console.log("autocompleteTextBoxNewItem onblur"); onBlurHandler(evnt)}}>
+    <div className={`autocompleteTextBoxNewItem${shouldShow ? '' : ' hidden'}`} style={{width:parentWidth, height:parentHeight}}>
+      <div className="autocompleteTextBoxNewItem__container">
         <div ref={txtBoxRef} className="autocompleteTextBoxNewItem__text" contentEditable="true" onInput={evnt => newItemUpdatedEventHandler(evnt)}></div>
         <ul className="autocompleteTextBoxNewItem__list">
           {newItemsOptions.map(
             (item, index) => {
               return (
-                <li key={index} className="autocompleteTextBoxNewItem__listitem" onClick={evnt => selectNewItemHandler(item)}>{item}</li>
+                <li key={index} className="autocompleteTextBoxNewItem__listitem" onClick={evnt => {console.log("autocompleteTextBoxNewItem__listitem onClick"); selectNewItemHandler(item)}}>{item}</li>
               );
             }
           )}
